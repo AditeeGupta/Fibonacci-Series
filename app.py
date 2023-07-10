7178061ad88638f74d782be169ffce0c388d0b33
AditeeGupta <133675389+AditeeGupta@users.noreply.github.com>
2023-07-10 15:59:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import openai
+import streamlit as st
+from streamlit_chat import message
+openai.api_key = st.secrets['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']
+def generate_response(prompt):
+    completions = openai.Completion.create(
+    model='davinci:ft-personal-2023-07-08-14-36-57',
+    prompt = prompt,
+    max_tokens = 1024,
+    n = 1,
+    stop = None,
+    temperature = 0.5
+    )
+    message = completions.choices[0].text
+    return message
+st.title("Chatbot")
 
